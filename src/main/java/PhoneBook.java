aa9beec67a4a59d1b0ee1bb6bdef9a8a1e78d6a6
Pavel Vnuchkov <pavelvnuckov.com>
2024-11-19 10:02:42 +0300

    add implemented

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -1,6 +1,13 @@
+import java.util.TreeMap;
+
 public class PhoneBook {
 
+    private TreeMap<String, String> phoneBook = new TreeMap<>();
+
     public int add(String name, String number) {
-        return 0;
+        if (!phoneBook.containsKey(name)) {
+            phoneBook.put(name, number);
+        }
+        return phoneBook.size();
     }
 }
