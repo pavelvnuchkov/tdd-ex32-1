ae5641ecdc185d5bfc2d245a927f4204b4e95936
Pavel Vnuchkov <pavelvnuckov.com>
2024-11-19 10:11:36 +0300

    Stub and tests for findByNumber

--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -6,6 +6,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class PhoneBookTest {
     private static final PhoneBook PHONE_BOOK = new PhoneBook();
+
     @ParameterizedTest
     @CsvSource(value = {
             "Иванов Петя, 89273331122, 1",
@@ -15,5 +16,15 @@ class PhoneBookTest {
     public void addTest(String name, String number, int expected) {
         int actual = PHONE_BOOK.add(name, number);
         Assertions.assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {
+            "89273331122, Иванов Петя",
+            "89224413311, Петров Женя",
+            "99999999999, Ничего не найдено"
+    })
+    public void findByNumber(String number, String expected) {
+        Assertions.assertEquals(expected, PHONE_BOOK.findByNumber(number));
     }
 }
